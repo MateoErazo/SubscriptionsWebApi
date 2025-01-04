a5ecb951280aa887f0945d4f0d1784bdf119d29f
Jesús Mateo <erazojesusmateo@gmail.com>
2025-01-03 21:24:59 -0500

    I add CRUD for ip restrictions controller

---
+++ SubscriptionsWebApi/DTOs/IPRestrictionCreationDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SubscriptionsWebApi.DTOs
+{
+  public class IPRestrictionCreationDTO
+  {
+    [Required]
+    public int APIKeyId { get; set; }
+    [Required]
+    public string IP { get; set; }
+  }
+}
