6875c9b2559971629d624a1186070da48ecee54c
Jesús Mateo <erazojesusmateo@gmail.com>
2025-01-04 19:01:51 -0500

    I add store procedure execution logic for invoices

---
+++ SubscriptionsWebApi/Services/InvoicesHostedService.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace SubscriptionsWebApi.Services
+{
+  public class InvoicesHostedService : IHostedService
+  {
+    private readonly IServiceProvider serviceProvider;
+    private Timer timer;
+
+    public InvoicesHostedService(IServiceProvider serviceProvider)
+    {
+      this.serviceProvider = serviceProvider;
+    }
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+      timer = new Timer(ProcessInvoices,null,TimeSpan.Zero,TimeSpan.FromDays(1));
+      return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+      timer.Dispose();
+      return Task.CompletedTask;
+    }
+
+    private void ProcessInvoices(object state)
+    {
+      using (var scope = serviceProvider.CreateScope())
+      {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        DateTime today = DateTime.Today;
+        DateTime comparisonDate = today.AddMonths(-1);
+        bool invoicesForTheMonthHaveBeenIssued = dbContext.IssuedInvoices
+          .Any(x => x.Year == comparisonDate.Year && x.Month == comparisonDate.Month);
+
+        if (!invoicesForTheMonthHaveBeenIssued)
+        {
+          DateTime startDate = new DateTime(comparisonDate.Year,comparisonDate.Month,1);
+          DateTime endDate = startDate.AddMonths(1);
+          dbContext.Database.ExecuteSqlInterpolated($"exec InvoicesCreation {startDate.ToString("yyyy-MM-dd")}, {endDate.ToString("yyyy-MM-dd")}");
+        }
+      }
+    }
+  }
+}
