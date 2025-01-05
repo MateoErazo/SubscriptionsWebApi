aa3d46c6392030895a12c5681f8ee0724e5e13ea
Jesús Mateo <erazojesusmateo@gmail.com>
2025-01-04 20:19:06 -0500

    I add logic to execute SP about set non paying user

--- SubscriptionsWebApi/Services/InvoicesHostedService.cs
+++ SubscriptionsWebApi/Services/InvoicesHostedService.cs
@@ -29,17 +29,28 @@ namespace SubscriptionsWebApi.Services
       using (var scope = serviceProvider.CreateScope())
       {
         var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        DateTime today = DateTime.Today;
-        DateTime comparisonDate = today.AddMonths(-1);
-        bool invoicesForTheMonthHaveBeenIssued = dbContext.IssuedInvoices
-          .Any(x => x.Year == comparisonDate.Year && x.Month == comparisonDate.Month);
+        SetNonPayingUsers(dbContext);
+        IssueInvoices(dbContext);
+      }
+    }
 
-        if (!invoicesForTheMonthHaveBeenIssued)
-        {
-          DateTime startDate = new DateTime(comparisonDate.Year,comparisonDate.Month,1);
-          DateTime endDate = startDate.AddMonths(1);
-          dbContext.Database.ExecuteSqlInterpolated($"exec InvoicesCreation {startDate.ToString("yyyy-MM-dd")}, {endDate.ToString("yyyy-MM-dd")}");
-        }
+    private static void SetNonPayingUsers(ApplicationDbContext dbContext)
+    {
+      dbContext.Database.ExecuteSqlInterpolated($"exec SetNonPayingUser");
+    }
+
+    private static void IssueInvoices(ApplicationDbContext dbContext)
+    {
+      DateTime today = DateTime.Today;
+      DateTime comparisonDate = today.AddMonths(-1);
+      bool invoicesForTheMonthHaveBeenIssued = dbContext.IssuedInvoices
+        .Any(x => x.Year == comparisonDate.Year && x.Month == comparisonDate.Month);
+
+      if (!invoicesForTheMonthHaveBeenIssued)
+      {
+        DateTime startDate = new DateTime(comparisonDate.Year, comparisonDate.Month, 1);
+        DateTime endDate = startDate.AddMonths(1);
+        dbContext.Database.ExecuteSqlInterpolated($"exec InvoicesCreation {startDate.ToString("yyyy-MM-dd")}, {endDate.ToString("yyyy-MM-dd")}");
       }
     }
   }
